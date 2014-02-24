875120cf6fcaaf3d5a9f177947836d9b22632d08
Anna Shipman <anna@annashipman.co.uk>
2014-02-24 16:16:51 +0000

    Add basic test case

--- test/majorityjudgement.py
+++ test/majorityjudgement.py
@@ -2,8 +2,18 @@ import unittest
 
 class MajorityJudgementTest(unittest.TestCase):
 
-    def test_1(self):
-        self.assertTrue(True)
+    def test_basic_small_case(self):
+        pizza = ('Pizza', (3,1,1))
+        burger = ('Burger', (2,0,3))
+        veggie = ('Veggie', (1,2,2))
+
+        input_data = (pizza, burger, veggie)
+
+        expected_output = (pizza, veggie, burger)
+
+        actual_output = MajorityJudgement().sortCandidates(input_data)
+        self.assertEqual(expected_output, actual_output)
+
 
 if __name__ == '__main__':
     unittest.main()
