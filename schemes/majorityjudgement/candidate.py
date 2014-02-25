e789fcd4546aa3bef123462045d1f8573592d58e
Steven Anderson <steve@whilefalse.net>
2014-02-25 11:10:06 +0000

    A bit of tidying up

--- schemes/majorityjudgement/candidate.py
+++ schemes/majorityjudgement/candidate.py
@@ -10,11 +10,12 @@ class Candidate(object):
     def _get_majority_value(self, tally):
         total_votes = sum(tally)
         votes_so_far = 0
+        vote_cutoff = (total_votes / 2 - 1) if total_votes % 2 == 0 else (total_votes - 1) / 2
+
         for i, tally_item in enumerate(tally):
             votes_so_far += tally_item
-            if (total_votes % 2 == 1 and votes_so_far > (total_votes-1)/2) \
-                or (total_votes % 2 == 0 and votes_so_far > (total_votes/2)-1):
-                    return [i] + self._get_majority_value(self._tally_with_ith_decremented(tally, i))
+            if votes_so_far > vote_cutoff:
+                return [i] + self._get_majority_value(self._tally_with_ith_decremented(tally, i))
         return []
 
     def _tally_with_ith_decremented(self, tally, i):
