9a62c3eaf9c531d58983c81f0ed91f4295439fd4
Dominic Baggott <dominic.baggott@gmail.com>
2014-02-27 14:22:24 +0000

    Store all rounds instead of only the latest

--- schemes/singletransferablevote/scheme.py
+++ schemes/singletransferablevote/scheme.py
@@ -183,26 +183,35 @@ class SingleTransferableVoteScheme(object):
         self.original_candidates = candidates
         self.remaining_candidates = candidates
         self.votes = votes
+        self.rounds = []
 
     def run_round(self):
-        self.latest_round = Round(self.num_vacancies, self.remaining_candidates, self.votes)
-        self.latest_round.run()
+        new_round = Round(self.num_vacancies, self.remaining_candidates, self.votes)
+        new_round.run()
 
         #import ipdb; ipdb.set_trace()
         self.remaining_candidates = filter(
-            lambda candidate: not candidate in self.round_results()['excluded'].keys(),
+            lambda candidate: not candidate in new_round.results()['excluded'].keys(),
             self.remaining_candidates
         )
 
+        self.rounds.append(new_round)
+
+    def latest_round(self):
+        if len(self.rounds) > 0:
+            return self.rounds[-1]
+
     def round_results(self):
-        return self.latest_round.results()
+        if len(self.rounds) > 0:
+            return self.latest_round().results()
 
     def completed(self):
-        return self.latest_round.all_vacancies_filled()
+        if len(self.rounds) > 0:
+            return self.latest_round().all_vacancies_filled()
 
     def final_results(self):
         if self.completed():
-            return self.latest_round.elected_candidates()
+            return self.latest_round().elected_candidates()
 
 class __SingleTransferableVoteScheme(object):
     """"""
