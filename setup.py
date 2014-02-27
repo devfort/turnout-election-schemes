0dc50435d9d17268bb6a53d0a4877d73ea5f9e59
Steven Anderson <steve@whilefalse.net>
2014-02-27 14:42:16 +0000

    Made it a pypi package

---
+++ setup.py
@@ -0,0 +1,30 @@
+# Use setuptools if we can
+try:
+    from setuptools.core import setup
+except ImportError:
+    from distutils.core import setup
+
+PACKAGE = 'pebblebox_election_schemes'
+VERSION = '0.1'
+
+setup(
+    name=PACKAGE, version=VERSION,
+    description="Contains election schemes for pebblebox.",
+    packages=[
+        'pebblebox_election_schemes',
+    ],
+    package_data={},
+    license='MIT',
+    author='/dev/fort 9',
+    author_email='contact@devfort.com',
+    install_requires=[
+    ],
+    url = 'https://github.com/devfort/pebblebox_election_schemes',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
